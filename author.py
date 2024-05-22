87acb64f19207519caac39342ff5098051ee35e7
Andy <ydnawizard@protonmail.com>
2024-05-22 17:12:52 -0500

    Added vector permutation functionality

--- author.py
+++ author.py
@@ -29,42 +29,7 @@ def generator(dim):
             vector.append(-1)
             x = random.randint(1,2)
             i+= 1
-    hypervector = np.array(vector)
-    print(countOf(hypervector, 1))
-    print(hypervector)
-    return hypervector
-
-def bundle(vector_a, vector_b):
-    vector_c = generator(10000)
-    vector_o = []
-    i=0
-    while i < len(vector_a) and i < len(vector_b):
-        if vector_a[i] == vector_b[i]:
-            vector_o.append(vector_a[i])
-            i+=1
-        else:
-            vector_o.append(vector_c[i])
-            i+=1
-    output = np.array(vector_o)
-    print(countOf(output, 1))
-    print(output)
-    return output
-
-def bind(vector_a, vector_b):
-    vector_o = []
-    i=0
-    j=0
-    while i < len(vector_a) and i < len(vector_b):
-        j = vector_a[i] * vector_b[i]
-        vector_o.append(j)
-        i+=1
-    output = np.array(vector_o)
-    print(output)
-    return output
-
-def permute(vector):
-    output = random.shuffle(vector)
-    print(output)
+    output = np.array(vector)
     return output
 
 #Assign atomic hypervectors to each character of
@@ -97,11 +62,61 @@ alphabet = {"a": generator(10000),
             "y": generator(10000),
             "z": generator(10000)}
 
+#Vector Bundler
+#Takes in two vectors and performs thresholded component-wise addition
+def bundle(vector_a, vector_b):
+    vector_c = generator(10000)
+    vector_o = []
+    i=0
+    while i < len(vector_a) and i < len(vector_b):
+        if vector_a[i] == vector_b[i]:
+            vector_o.append(vector_a[i])
+            i+=1
+        else:
+            vector_o.append(vector_c[i])
+            i+=1
+    output = np.array(vector_o)
+    print(countOf(output, 1))
+    print(output)
+    return output
+
+#Vector Binder
+#Takes in two vectors and performs component-wise multiplication
+def bind(vector_a, vector_b):
+    vector_o = []
+    i=0
+    j=0
+    while i < len(vector_a) and i < len(vector_b):
+        j = vector_a[i] * vector_b[i]
+        vector_o.append(j)
+        i+=1
+    output = np.array(vector_o)
+    print(output)
+    return output
+
+#Permutation Matrix Declaration
+#Takes in n returns a permutation matrix of size nxn
+def permute_generator(n):
+    rho = np.eye(n)
+    np.random.shuffle(rho)
+    print(rho)
+    return rho
+
+
+#Vector Permuter
+#Takes in a vector and a permutation matrix, then mutiplies the vector by the permutation matrix
+def permute(vector, rho):
+    vector_p = np.matmul(vector, rho)
+    return vector_p
+
+
 def main():
     inp = input()
     x = generator(int(inp))
     y = generator(int(inp))
-    cosine = permute(x)11
+    print(x)
+    rho = permute_generator(int(inp))
+    print(permute(x, rho))
 
 
 if __name__ == "__main__":
